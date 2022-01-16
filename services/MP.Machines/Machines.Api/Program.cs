d5c24f634b6695b6b6d36e07c54d0e882a0fff41
kamil <pankowski.kamil1989@gmail.com>
2022-01-16 14:56:53 +0100

    WIP: Move services from program.cs to extensions

--- services/MP.Machines/Machines.Api/Program.cs
+++ services/MP.Machines/Machines.Api/Program.cs
@@ -1,25 +1,17 @@
-// using System.Text.Json.Serialization;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using Machines.Api.Validators;
+using Machines.Api.Extensions;
 using Machines.DataAccess.EfCore;
-using Machines.DataAccess.EfCore.Repositories;
 using Machines.DataAccess.EfCore.Services.DB;
-using Machines.DataAccess.EfCore.UnitOfWork;
 using Machines.Domain.Configuration.Options;
-using Machines.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using MP.MachinesApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// .AddJsonOptions(options => 
-//     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -34,19 +26,11 @@ builder.Services.Configure<SeedOptions>
     (builder.Configuration.GetSection(SeedOptions.Seed));
 # endregion
 
-builder.Services.AddMvc().AddFluentValidation();
-
-# region Repositories
-builder.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddTransient<IParameterRepository, ParameterRepository>();
-builder.Services.AddTransient<IMachineRepository, MachineRepository>();
+# region Extensions
+builder.Services.RepoServiceExtensions();
+builder.Services.DbServiceExtensions();
+builder.Services.ValidatorsServiceExtensions();
 # endregion
-
-builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IDbInitialize, DbInitialize>();
-builder.Services.AddScoped<IDbSeed, DbSeed>();
-builder.Services.AddTransient<IValidator<Parameter>, ParameterValidator>();
-builder.Services.AddTransient<IValidator<Machine>, MachineValidator>();
 
 var app = builder.Build();
 
